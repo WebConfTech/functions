6b480e4e6249f68107cf64826ec06fcbaacd6feb
Ata Sanchez <gahs94@gmail.com>
2019-02-20 20:19:08 -0300

    Small fix in package.json

--- lambdas/mail_send.js
+++ lambdas/mail_send.js
@@ -2,13 +2,12 @@ if (process.env.NODE_ENV === 'development') {
   require('dotenv').config();
 }
 
-const mailgun = require('mailgun-js');
-
 const domain = process.env.MAILGUN_DOMAIN;
 const apiKey = process.env.MAILGUN_API_KEY;
-const listName = process.env.MAILGUN_LIST;
 
-const mg = mailgun({ apiKey: api_key, domain: DOMAIN });
+const mailgun = require('mailgun-js');
+
+const mg = mailgun({ apiKey, domain });
 const data = {
   from: 'WebConf <no-reply@webconf.tech>',
   to: 'gahs94@gmail.com',
