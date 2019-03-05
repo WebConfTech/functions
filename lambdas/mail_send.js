a328129de977d179261e369e79332aaa7f752d0d
Santiago Persico <santiagopersico@gmail.com>
2019-03-05 04:31:39 -0300

    implements initial approach to email sender microservice

--- lambdas/mail_send.js
+++ lambdas/mail_send.js
@@ -4,17 +4,22 @@ if (process.env.NODE_ENV === 'development') {
 
 const domain = process.env.MAILGUN_DOMAIN;
 const apiKey = process.env.MAILGUN_API_KEY;
-
+const fs = require('fs')
 const mailgun = require('mailgun-js');
-
-const mg = mailgun({ apiKey, domain });
-const data = {
-  from: 'WebConf <no-reply@webconf.tech>',
-  to: 'gahs94@gmail.com',
-  subject: 'WebConf • Nuestro newsletter 🙈',
-  text: 'Hola perrix! :D'
-};
+const mjml2html = require('mjml')
 
 module.exports = (req, res) => {
-  mg.messages().send(data, (error, body) => console.log(body));
+  fs.readFile(__dirname + './../templates/mail_registration.mjml', 'utf8', function(err, file) {
+    mailgun({ domain, apiKey })
+      .messages()
+      .send(
+        {
+          from: 'WebConf <no-reply@webconf.tech>',
+          to: 'persi93@gmail.com',
+          subject: 'WebConf • Nuestro newsletter 🙈',
+          html: mjml2html(file).html
+        },
+        (error, body) => res.end(JSON.stringify({ error, body }))
+      );
+  })
 };
