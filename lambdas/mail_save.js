e23fd9dc772391129434ba181690f91481edf445
homer0 <leo.apiwan@olapic.com>
2019-03-13 01:40:11 -0300

    Remove debug line

--- lambdas/mail_save.js
+++ lambdas/mail_save.js
@@ -54,7 +54,6 @@ const handler = async (req, res) => {
           if (err) {
             send(res, statuses['bad request'], err.message);
           } else {
-            console.log('ENV', require('../env'));
             send(
               res,
               statuses['created'],
