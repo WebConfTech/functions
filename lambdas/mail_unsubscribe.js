91afe39b49d4fc76fb9fc967deac611fa150021e
homer0 <leo.apiwan@olapic.com>
2019-03-13 01:35:33 -0300

    Clean up the unsubscribe function

--- lambdas/mail_unsubscribe.js
+++ lambdas/mail_unsubscribe.js
@@ -12,25 +12,26 @@ module.exports = async (req, res) => {
   try {
     const data = await json(req);
     if (!(data && data['event-data'] && data['event-data'].recipient)) {
-      console.error('INVALID DATA', data);
-      return send(res, statuses['not acceptable'], {});
+      return send(res, statuses['not acceptable'], {
+        message: 'Invalid payload',
+      });
     }
 
     if (!mg.validateWebhook(data.signature.timestamp, data.signature.token, data.signature.signature)) {
-      console.error('INVALID SIGNATURE', data.signature);
-      return send(res, statuses['not allowed'], {});
+      return send(res, statuses['not allowed'], {
+        message: 'Invalid signature',
+      });
     }
 
-    console.log('UNSUBSCRIBE', data['event-data'].recipient, 'FROM', listName);
     const deletion = await mg
       .lists(listName)
       .members(data['event-data'].recipient)
       .delete();
 
-    console.log('SUCCESS?', deletion);
   } catch (error) {
-    console.error('UNEXPECTED ERROR', error)
-    return send(res, statuses['not acceptable'], {});
+    return send(res, statuses['not acceptable'], {
+      message: `Unexpected error: ${error.message}`,
+    });
   }
 
   return send(res, statuses['ok'], {});
