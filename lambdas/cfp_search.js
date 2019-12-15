e9bd807b2df43b45528c88d616396ac5603cfa24
homer0 <leo.apiwan@olapic.com>
2019-12-15 03:26:35 -0300

    feat: search the CFP tweets using the options from firebase

--- lambdas/cfp_search.js
+++ lambdas/cfp_search.js
@@ -1,20 +1,37 @@
 const statuses = require('statuses');
 const { send } = require('micro');
-const { getClient, searchTweets } = require('../lib/twitter');
-
-const client = getClient();
+const { searchTweets } = require('../lib/twitter');
+const {
+  OPTIONS,
+  getOptions,
+  updateLastResultId,
+} = require('../lib/cfp');
 
 module.exports = async (req, res) => {
   try {
-    const tweets = await searchTweets({
-      query: '#apple',
-    });
+    const useOptions = await getOptions();
+    const searchOptions = {
+      query: useOptions[OPTIONS.SEARCH_VALUE].value,
+    };
+
+    let lastResultIdDoc = null;
+    if (useOptions[OPTIONS.LAST_RESULT_ID]) {
+      searchOptions.sinceId = Number(useOptions[OPTIONS.LAST_RESULT_ID].value);
+      lastResultIdDoc = useOptions[OPTIONS.LAST_RESULT_ID].doc;
+    }
+    
+    const tweets = await searchTweets(searchOptions);
+    if (tweets.length) {
+      const [{ id_str: lastResultId }] = tweets;
+      await updateLastResultId(lastResultId, lastResultIdDoc);
+    }
+
     res.end(JSON.stringify({ tweets: tweets.length }));
   } catch (error) {
     return send(
       res,
       statuses['bad request'],
-      JSON.stringify({ error: `Error on Twitter search: ${err.message}` })
+      JSON.stringify({ error: `Error on Twitter search: ${error.message}` })
     );
   }
 };
